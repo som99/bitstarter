86a59726cc59b15bf767ed116ef45cca6686d567
iamsom <iamsom@gmail.com>
2013-07-24 16:42:50 +0530

    Modified web.js again

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fh = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from Som!');
+  response.send(fh.writeFileSync('index.html',utf8));
 });
 
 var port = process.env.PORT || 5000;
